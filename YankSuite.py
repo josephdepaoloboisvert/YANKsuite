2327e0e1f5e45b12552f6036c82c3628c6a24e77
josephdepaoloboisvert <joseph184depaolo@gmail.com>
2021-12-21 11:50:55 -0500

    Changed complex and solvent to phase1 and phase 2 Began adding framework for repeats Continuing to port over old code

--- YankSuite.py
+++ YankSuite.py
@@ -7,23 +7,42 @@ import netCDF4 as netcdf
 import sys
 from yank.experiment import *
 
-class YankAnalyzer:
+class YankMultiAnalyzer():
+    """A Class for comparing multiple YankAnalyzers"""
+    def __init__(self, in_dictionary):
+        """Format of in_dictionary is keys = string name; value = list of YankAnalyzer objects that are repeats of each other
+        EX: Protein_in_dict = {'LIG1': [YankAnalyzerObjectFromRepeat1, YankAnalyzerObjectFromRepeat2, YankAnalyzerObjectFromRepeat3], etc... }
+        Every value-list should have at least two entries and the overall dictionary should have at least one entry."""
+
+
+    def convergence_of_rpts(self):
+        """Analyze the convergence of repeats (see init for how repeats are defined)"""
+
+class YankAnalyzer():
     def __init__(self, full_path_to_yaml):
+        """
+        Python class for managing the information exracted from YANK simulations
+
+        PARAMETERS:
+        full_path_to_yaml: full path to the yaml file which ran the simulation type(str)
+
+        RETURNS:
+            does not return
+        """
         #Get to directory that contains simulation
         idx = [index for index, element in enumerate(full_path_to_yaml) if element == '/']
         self.yank_dir = full_path_to_yaml[:idx[-1]+1]  # Trailing Slash included
-        this_dir = os.getcwd()
-        os.chdir(self.yank_dir)
-
-        #Obtain Contents of generate yaml file ($OUTPUT/experiments/experiments.yaml)
-        f = open(full_path_to_yaml[idx[-1] + 1:], 'r')
+        f = open(full_path_to_yaml, 'r')
         yaml_script_contents = [line[:-2] if line.endswith('\n') else line for line in f.readlines()]
         f.close()
+
+        # Obtain Contents of generate yaml file ($OUTPUT/experiments/experiments.yaml)
         try:
-            self.out_dir = get_from_yaml('output_dir:', yaml_script_contents)
+            self.out_dir = self.yank_dir + get_from_yaml('output_dir:', yaml_script_contents)
         except:
-            self.out_dir = 'output'
-        f = open(self.yank_dir + '%s/experiments/experiments.yaml'%self.out_dir, 'r')
+            self.out_dir = self.yank_dir + 'output'
+
+        f = open(self.out_dir + '/experiments/experiments.yaml', 'r')
         self.yaml_contents = [line[:-2] if line.endswith('\n') else line for line in f.readlines()]
         f.close()
 
@@ -33,55 +52,75 @@ class YankAnalyzer:
         except:
             self.dsl_lig = 'resname LIG'
         try:
-            self.s_complex = list(get_from_yaml('phase1_path', self.yaml_contents))
+            self.s_phase1 = [self.yank_dir + element for element in get_from_yaml('phase1_path', self.yaml_contents).strip('][').split(', ')]
         except:
-            self.s_complex = ['complex.prmtop','complex.inpcrd']
+            self.s_phase1 = [self.yank_dir + element for element in ['complex.prmtop','complex.inpcrd']]
         try:
-            self.s_solvent = list(get_from_yaml('phase2_path', self.yaml_contents))
+            self.s_phase2 = [self.yank_dir + element for element in get_from_yaml('phase2_path', self.yaml_contents).strip('][').split(', ')]
         except:
-            self.s_solvent = ['solvent.prmtop', 'solvent.inpcrd']
-
-        self.yaml = YankLoader(self.yank_dir + '%s/experiments/experiments.yaml'%self.out_dir)  # Probably
-        self.complex_nc = netcdf.Dataset(self.yank_dir + '%s/experiments/complex.nc'%self.out_dir)  # Probably
-        self.solvent_nc = netcdf.Dataset(self.yank_dir + '%s/experiments/solvent.nc' % self.out_dir)  # Probably
-
-        #MDA Universes reference (input files), complex(complex.nc -> state0), alchemical(complex.nc -> state-1),
-        # solvent(solvent.nc -> state0), vacuum(solvent.nc -> state-1)
-        self.ref = mda.Universe(self.s_complex[0], self.s_complex[1])
-        # self.complex = mda.Universe(self.s_complex[0], self.s_complex[1])
-        # self.alchemical = mda.Universe(self.s_complex[0], self.s_complex[1])
-        # self.solvent = mda.Universe(self.s_solvent[0], self.s_solvent[1])
-        # self.vacuum = mda.Universe(self.s_solvent[0], self.s_solvent[1])
+            self.s_phase2 = [self.yank_dir + element for element in ['solvent.prmtop','solvent.inpcrd']]
 
         # Setting up directory that data will be stored in
         for dir in ['trajs', 'overlap_matrices', 'rsmds']:
-            if not os.path.exists('./%s/%s'%(self.out_dir, dir)):
-                os.mkdir('./%s/%s'%(self.out_dir, dir))
+            if not os.path.exists('%s/%s'%(self.out_dir, dir)):
+                os.mkdir('%s/%s'%(self.out_dir, dir))
 
+        self.yaml = YankLoader(self.out_dir + '/experiments/experiments.yaml')  # Probably
+        self.phase1_nc = netcdf.Dataset(self.out_dir + '/experiments/complex.nc')  # Probably
+        self.phase2_nc = netcdf.Dataset(self.out_dir + '/experiments/solvent.nc')  # Probably
 
-    def extract_trajs(self, state_string, state_index, sel_str_store, memory=False):
-        # if memory, this will also return to an MDA universe of the trajectory
+        #MDA Universes reference (input files), complex(complex.nc -> state0), alchemical(complex.nc -> state-1), solvent(solvent.nc -> state0), vacuum(solvent.nc -> state-1)
+        self.ref = mda.Universe(self.yank_dir+self.s_phase1[0], self.s_phase2[1])
+
+        # extract trajectories
+        self.extract_traj('complex', 0, 'all')
+        self.extract_traj('complex', -1, 'protein')
+        self.extract_traj('complex', -1, self.dsl_lig)
+        self.extract_traj('solvent', 0, 'all')
+        self.extract_traj('solvent', -1, self.dsl_lig)
+
+    def extract_traj(self, state_string, state_index, sel_str_store, sel_str_align):
+        """
+        get a thermodynamic state of the yank simulation
+        :param state_string: must be either the string 'complex' or 'solvent'
+        :param state_index: replica/state index to extract (typically 0 or -1)
+        :param sel_str_store: MDAnalysis selection string to store trajectory of
+        :param memory: if memory, the extracted trajectory will be returned as an MDAnalysis Universe Object
+
+        extract_trajs('complex', 0, 'all')
+        extract_trajs('complex', -1, 'protein')
+        extract_trajs('complex', -1, self.dsl_lig)
+        extract_trajs('solvent', 0, 'all')
+        extract_trajs('solvent', -1, self.dsl_lig)
+        """
         if state_string == 'complex':
-            nc = self.complex_nc
+            nc = self.phase1_nc
+            temp_u = mda.Universe(self.s_phase1[0], self.s_phase1[1])
         elif state_string == 'solvent':
-            nc = self.solvent_nc
+            nc = self.phase2_nc
+            temp_u = mda.Universe(self.s_phase2[0], self.s_phase2[1])
         else:
             raise Exception('state_string must be either complex or solvent')
 
         niterations = nc.variables['positions'].shape[0]
         nstates = nc.variables['states'].shape[1]
         natoms = nc.variables['positions'].shape[2]
-        replica_indices = [list(nc.variables['states'][iteration, :]).index(0) for iteration in range(0, niterations)]
+
+
+        i_state = state_index
+        if state_index == -1:
+            i_state = nstates-1
+
+        replica_indices = [list(nc.variables['states'][iteration, :]).index(i_state) for iteration in range(0, niterations)]
 
         # Align and store snapshots
-        temp_u = MDA.universe(self.s_complex[0], self.s_complex[1])
         sel_store = temp_u.select_atoms(sel_str_store)
-        store_dcd = './%s/trajs/%s_%s_%s.dcd'%(self.out_dir, state_string, state_index, sel_str_store)
+        store_dcd = '%s/trajs/%s_%s_%s.dcd'%(self.out_dir, state_string, i_state, sel_str_store)
         writer = mda.Writer(store_dcd, sel_store.n_atoms)
         for frame in range(0, len(replica_indices)):
             coords = nc.variables['positions'][frame, replica_indices[frame], :, :] * 10.0
             temp_u.load_new(coords, format=MemoryReader)
-            align.alignto(temp_u, self.ref, select='name CA')
+            align.alignto(temp_u, temp_u, select=sel_str_align)
             writer.write(sel_store)
 
         # Write a PDB file with the stored atoms, storing AMBER atom types as a remark
@@ -94,13 +133,13 @@ class YankAnalyzer:
         sel_store.write(store_dcd.replace('.dcd','.pdb'), remarks=remarks)
 
 
-    def graph_overlap_matrix(self, state):
-        if state == 'complex':
-            nc = self.complex_nc
-        elif state == 'solvent':
-            nc = self.solvent_nc
+    def graph_overlap_matrix(self, state_string):
+        if state_string == 'complex':
+            nc = self.phase1_nc
+        elif state_string == 'solvent':
+            nc = self.phase2_nc
         else:
-            raise Exception('State must be either complex or solvent')
+            raise Exception('state_string must be either complex or solvent')
         proposed = np.array(tuple([np.array(nc.variables['proposed'][i, :, :]) for i in range(nc.variables['positions'].shape[0])]))
         accepted = np.array(tuple([np.array(nc.variables['accepted'][i, :, :]) for i in range(nc.variables['positions'].shape[0])]))
         matrix = np.sum(accepted, axis=0) / np.sum(proposed, axis=0)
@@ -112,6 +151,9 @@ class YankAnalyzer:
 
 
     def graph_rmsds(self):
+
+
+    def get_GSBA_energies(self):
 
 
 def get_from_yaml(name, yaml_contents):
