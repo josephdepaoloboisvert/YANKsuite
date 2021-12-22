069c6cf410e128c00044d288460865bcf4b071b7
josephdepaoloboisvert <joseph184depaolo@gmail.com>
2021-12-22 17:03:35 -0500

    Extracting Trajs and loading them in

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # YANKsuite
+
+from YankSuite import *
+
+Proposed Workflow
+
+sim = YankAnalyzer("full/path/to/yaml/that/ran/YANK/simulation.yaml")
--- YankSuite.py
+++ YankSuite.py
@@ -1,11 +1,14 @@
 import yaml
 import matplotlib.pyplot as plt
 import MDAnalysis as mda
+import yank.analyze
 from MDAnalysis.coordinates.memory import MemoryReader
+from MDAnalysis.analysis import rms
 from MDAnalysis.analysis import align
 import netCDF4 as netcdf
 import sys
 from yank.experiment import *
+from yank.analyze import *
 
 class YankMultiAnalyzer():
     """A Class for comparing multiple YankAnalyzers"""
@@ -31,14 +34,14 @@ class YankAnalyzer():
         """
         #Get to directory that contains simulation
         idx = [index for index, element in enumerate(full_path_to_yaml) if element == '/']
-        self.yank_dir = full_path_to_yaml[:idx[-1]+1]  # Trailing Slash included
+        self.yank_dir = full_path_to_yaml[:idx[-1]+1]  # Trailing / included
         f = open(full_path_to_yaml, 'r')
         yaml_script_contents = [line[:-2] if line.endswith('\n') else line for line in f.readlines()]
         f.close()
 
         # Obtain Contents of generate yaml file ($OUTPUT/experiments/experiments.yaml)
         try:
-            self.out_dir = self.yank_dir + get_from_yaml('output_dir:', yaml_script_contents)
+            self.out_dir = self.yank_dir + get_from_yaml('output_dir:', yaml_script_contents) # No Trailing /
         except:
             self.out_dir = self.yank_dir + 'output'
 
@@ -61,7 +64,7 @@ class YankAnalyzer():
             self.s_phase2 = [self.yank_dir + element for element in ['solvent.prmtop','solvent.inpcrd']]
 
         # Setting up directory that data will be stored in
-        for dir in ['trajs', 'overlap_matrices', 'rsmds']:
+        for dir in ['trajs', 'overlap_matrices', 'rmsds']:
             if not os.path.exists('%s/%s'%(self.out_dir, dir)):
                 os.mkdir('%s/%s'%(self.out_dir, dir))
 
@@ -72,6 +75,7 @@ class YankAnalyzer():
         #MDA Universes reference (input files), complex(complex.nc -> state0), alchemical(complex.nc -> state-1), solvent(solvent.nc -> state0), vacuum(solvent.nc -> state-1)
         self.ref = mda.Universe(self.yank_dir+self.s_phase1[0], self.s_phase2[1])
 
+        #End Initialization with performing all actions that need the NC files in memory
         # extract trajectories
         self.extract_traj('complex', 0, 'all')
         self.extract_traj('complex', -1, 'protein')
@@ -79,9 +83,13 @@ class YankAnalyzer():
         self.extract_traj('solvent', 0, 'all')
         self.extract_traj('solvent', -1, self.dsl_lig)
 
+        #Make Replica Overlap Matrices
+        self.graph_overlap_matrix('complex')
+        self.graph_overlap_matrix('solvent')
+
     def extract_traj(self, state_string, state_index, sel_str_store, sel_str_align):
         """
-        get a thermodynamic state of the yank simulation
+        extract a thermodynamic state of the yank simulation,, storing the atoms in mdanalysis selection sel_str_store and aligning the trajectory on sel_str_align
         :param state_string: must be either the string 'complex' or 'solvent'
         :param state_index: replica/state index to extract (typically 0 or -1)
         :param sel_str_store: MDAnalysis selection string to store trajectory of
@@ -133,29 +141,80 @@ class YankAnalyzer():
         sel_store.write(store_dcd.replace('.dcd','.pdb'), remarks=remarks)
 
 
+    def load_traj(self, state_string, state_index, sel_str_store):
+        return mda.Universe('%s/trajs/%s_%s_%s.dcd'%(self.out_dir, state_string, state_index, sel_str_store))
+
+
     def graph_overlap_matrix(self, state_string):
-        if state_string == 'complex':
+        """
+        Plot the replica exchange matrix for either the phase1 or phase2 nc
+        :param state_string: must be either the string 'phase1' or 'phase2'
+        :return:
+        """
+        if state_string == 'phase1':
             nc = self.phase1_nc
-        elif state_string == 'solvent':
+        elif state_string == 'phase2':
             nc = self.phase2_nc
         else:
-            raise Exception('state_string must be either complex or solvent')
+            raise Exception("state_string must be either the string 'phase1' or 'phase2'")
+
         proposed = np.array(tuple([np.array(nc.variables['proposed'][i, :, :]) for i in range(nc.variables['positions'].shape[0])]))
         accepted = np.array(tuple([np.array(nc.variables['accepted'][i, :, :]) for i in range(nc.variables['positions'].shape[0])]))
         matrix = np.sum(accepted, axis=0) / np.sum(proposed, axis=0)
+
         plt.clf()
         plt.colorbar(plt.matshow(matrix))
         plt.xlabel('Replica i')
         plt.ylabel('Replica j')
-        plt.savefig('./%s/%s/%s.png'%(self.out_dir, 'overlap_matrices', state))
+        plt.savefig('%s/overlap_matrices/%s.png'%(self.out_dir, state_string))
 
 
-    def graph_rmsds(self):
+    def graph_rmsd(self, mda_universe, rmsd_select, save_name, align_string=None):
+        """
+        Saves a graph of the RMSD of rmsd_select over the simulation provided as an mda_universe
+        :param mda_universe: simulation to analyze (see self.load_traj())
+        :param rmsd_select: mdanalysis string of the group to obtain rmsd of
+        :param save_name: save name for the rmsd graph as string ending in .png
+        :param align_string: Perform alignment if desired (self.extract_traj() already aligns)
+        :return: Does not return; saves png to self.out_dir/rmsds/SAVE_NAME
+        """
+        sim = mda_universe
+
+        # Align each trajectory to the reference
+        if align_string is not None:
+            aligner = align.AlignTraj(sim, self.ref, select=align_string, in_memory=True).run()
+
+        # Evaluate the RMSD of GROUP_SELECT with respect to the reference frame
+        # Each [group]selection describes additional RMSDs to be computed after the structures
+        # have been superimposed according to select.
+        ref_select = self.ref.select_atoms(rmsd_select)
+        sim_select = sim.select_atoms(rmsd_select)
+        data = np.array([[ts.time, rms.rmsd(sim_select.positions, ref_select.positions)] for ts in sim.trajectory])
+
+        plt.clf()
+        plt.title('RMSD of %s'%rmsd_select)
+        plt.xlabel('Time')
+        plt.ylabel('RMSD (Angstroms)')
+        plt.savefig('%s/rmsds/%s.png'%(self.out_dir, save_name))
+
+    def YankBFEAnalysis(self):
+        """
+        Perform the YANK binding free energy analysis
+        :return:
+        """
+        analysis_yaml = '%s/experiments/analysis.yaml'%self.out_dir
+        analyzer = yank.analyze.ExperimentAnalyzer(analysis_yaml)
+        return analyzer.auto_analyze()
 
 
     def get_GSBA_energies(self):
+        raise Exception('Still under development')
 
 
+
+
+
+#Utility Functions
 def get_from_yaml(name, yaml_contents):
     truth_list = [True if name in line else False for line in yaml_contents]
     if True in truth_list:
